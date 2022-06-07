c69bc7f29b8798565eb738c2885f19e6307b5a66
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-07 18:05:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-
 // Write your code here
+
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62
+let num1
+let num2
+
+const multiply = function(num1, num2) {
+    return num1 * num2
+}
+
+// Create a variable called random that will generate a random integer greater than 0.
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+const mod = function(num3, num4) {
+    return num3 % num4;
+}
+mod(5, 2)
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+const max = 20;
+
+// Hii imekuwa ngumu, nimeshindwa
